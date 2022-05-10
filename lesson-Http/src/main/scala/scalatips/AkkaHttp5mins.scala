0858709b9e56598d7d2c8b33b8349a0fd13c53d2
Prokopenko Andrey <prokopenko_andrey@mail.ru>
2022-05-10 23:16:34 +0300

    sending http request with scala and Akka Http

---
+++ lesson-Http/src/main/scala/scalatips/AkkaHttp5mins.scala
@@ -0,0 +1,47 @@
+package scalatips
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.{Http, model}
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+
+import java.net.URLEncoder
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+object AkkaHttp5mins {
+
+  implicit val system = ActorSystem() //  Akka actors
+  implicit val materializer = ActorMaterializer() //  Akka stream
+  import system.dispatcher  //  "thread pool"
+
+  val source =
+    """
+      |object SimmpleApp {
+      | val aField = 2
+      |
+      | def aMethod(x: Int) = x + 1
+      |
+      | def main(argc: Array[String]: Unit = println(aField)
+      | }
+      """.stripMargin
+
+  val request = HttpRequest(
+    method = HttpMethods.POST,
+    uri = "http://markup.su/api/highlighter",
+    entity = HttpEntity(
+      ContentTypes.`application/x-www-form-urlencoded`,  //  application/json in most cases
+      s"source=${URLEncoder.encode(source,"UTF-8")}&language=Scala&theme=Sunburst"  //  the actual data you want to send
+    )
+  )
+
+  def sendRequest(): Future[String] = {
+    val responseFuture: Future[HttpResponse] = Http().singleRequest(request)
+    val entityFuture: Future[HttpEntity.Strict] = responseFuture.flatMap(response => response.entity.toStrict(2.seconds))
+    entityFuture.map(entity => entity.data.utf8String)
+  }
+
+  def main(argc: Array[String]): Unit = {
+    sendRequest().foreach(println)
+  }
+}
